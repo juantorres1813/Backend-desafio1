6dc7d3d75b19b144b0ad21dab2c17626949ec0fa
Juan Torres <juantorres1813@gmail.com>
2022-09-21 04:45:36 -0300

    entrega desafio 2

--- desafio2.js
+++ desafio2.js
@@ -1,53 +1,81 @@
 const fs = require('fs')
 
 class Contenedor{
-    constructor(archivo, title, price, thumbnail){
+    constructor(archivo){
         this.file = archivo
-        this.title = title
-        this.price = price
-        this.thumbnail = thumbnail
     }
 
     async getAll() {
         try {
-            const contenido = await fs.promises.readFile('./productos.txt', 'utf-8')
-            return contenido
+            const archivo = await fs.promises.readFile(this.file, 'utf-8')
+            return JSON.parse(archivo)
         }
         catch (error) {
             console.log("error de escritura", error)
+            return [];
         }
     }
 
-    async save() {  
-        const idfuncion = () => {
-            if (contenido.length> 1) {
-                let idRecibida = this.id
-                idRecibida = id++
+    async save(obj){
+        try {
+            const archivo = await this.getAll()
+            let nuevoId;
+            if (archivo.length === 0) {
+                nuevoId = 1;
             } else {
-                idRecibida = this.id
+                nuevoId = archivo[archivo.length - 1].id + 1;
             }
-        }
-        idfuncion()
-
-        console.log(JSON.stringify(idRecibida))
-        console.log(JSON.stringify(this.id))
-
-        // fs.appendFile('./productos.txt', JSON.stringify(idRecibida, null, 2), error => {
-        //     if (error) {
-        //         throw new Error(`Error en escritura: ${error}`)
-        //         }
-        //     else {
-        //         console.log("escritura exitosa")
-        //     }
-        // })
+            const productoConId = { ...obj, id: nuevoId }
+            archivo.push(productoConId);
+            await fs.promises.writeFile(this.file, JSON.stringify(archivo, null, 3) )
+            console.log("escrito correctamente")
+            return nuevoId
+            } catch (error) {
+                console.log(`Error en escritura `,error)
+            }
     }
-    
 
+    async getById(numero) {
+        try{
+            const archivo = await this.getAll()
+            if (numero != null) {
+                const foundItem = archivo.filter(x => x.id === Number(numero))
+                console.log(foundItem);
+                return foundItem
+            } else {
+                return null
+            }
+        } catch (error) {
+            console.log(`Error en escritura `,error)
+        }
+    }
+
+    async deleteById(numero) {
+        try{
+            const archivo = await this.getAll()
+            if (numero != null) {
+                const foundItem = archivo.filter((x) => x.id !== numero);
+                console.log('Elemento borrado exitosamente', foundItem);
+                await fs.promises.writeFile(this.file, JSON.stringify(foundItem, null, 3))
+            } else {
+                return null
+            }
+        } catch (error) {
+            console.log(`Error en escritura `,error)
+        }
+    }
+
+    async deleteAll() {
+        try{
+            const archivo = await this.getAll()
+            archivo.length = 0
+            await fs.promises.writeFile(this.file, JSON.stringify(archivo, null, 3))
+            console.log('Array borrado exitosamente')
+        } catch (error) {
+            console.log(`Error en escritura `,error)
+        }
+    }
 }
 
+module.exports = Contenedor
 
-const Contenedor1 = new Contenedor('./productos.txt');
-
-Contenedor1.getAll()
-
-Contenedor1.save({id : 1})
---
+++ test.js
@@ -0,0 +1,34 @@
+const Contenedor = require('./desafio2.js')
+
+const Contenedor1 = new Contenedor('./productos.txt');
+
+// Contenedor1.getAll().then( console.log )
+
+// Contenedor1.save({                                                                                                                                                    
+//     title: 'Globo Terráqueo',                                                                                                                          
+//     price: 345.67,                                                                                                                                     
+//     thumbnail: 'https://cdn3.iconfinder.com/data/icons/education-209/64/globe-earth-geograhy-planet-school-256.png',                                   
+//     } 
+// )
+
+// Contenedor1.save({                                                                                                                                                    
+//     title: 'Escuadra',                                                                                                                                 
+//     price: 123.45,                                                                                                                                     
+//     thumbnail: 'https://cdn3.iconfinder.com/data/icons/education-209/64/ruler-triangle-stationary-school-256.png',                                                                                                                                                                                 
+//     }
+// )
+
+// Contenedor1.save({                                                                                                                                                    
+//     title: 'Calculadora',                                                                                                                              
+//     price: 234.56,                                                                                                                                     
+//     thumbnail: 'https://cdn3.iconfinder.com/data/icons/education-209/64/calculator-math-tool-school-256.png',                                                                                                                                                                                      
+//     }
+// )
+
+
+
+// Contenedor1.getById(3)
+
+// Contenedor1.deleteById(1)
+
+// Contenedor1.deleteAll()
